ffa2410daba08a776d0dd30da60c5d052b65c90a
AgathaKarenne32 <agathakarenne32@gmail.com>
2026-03-30 16:45:34 +0000

    Estrutura Maven, Teste de Falha e Pipeline CI

---
+++ src/main/java/com/moneywise/App.java
@@ -0,0 +1,13 @@
+package com.moneywise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
